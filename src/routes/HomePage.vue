118f0785e02e4927841e6c82f7e7804f3875ca42
conho-web <conho@internet.ru>
2026-01-08 16:16:35 +0300

    init setup

--- src/routes/HomePage.vue
+++ src/routes/HomePage.vue
@@ -1,14 +1,16 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div :class="$style.wrapper">
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'HomePage'
 }
 </script>
 
 <style lang="scss" module>
+  .wrapper {
+  }
 </style>
