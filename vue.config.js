1fe6cf7c66772f545c12972dd3b7774ec604812e
conho-web <conho@internet.ru>
2026-01-08 14:19:01 +0300

    test new deploy

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
-  build: {
-    outDir: 'docs'
-  }
+  outputDir: './docs'
 })
